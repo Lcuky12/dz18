b0e0e285e0c39780606ee57bf97f651f51327fe7
Lcuky12 <118757873+Lcuky12@users.noreply.github.com>
2023-01-20 23:12:34 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int grandmothersLine;
+            int onePersonTimeMinute = 10;
+            int hourLine;
+            int minuteLine;
+            int minute = 60;
+
+            Console.Write(" Введите кол-во людей ");
+            grandmothersLine = Convert.ToInt32(Console.ReadLine());
+            hourLine = grandmothersLine * onePersonTimeMinute / minute;
+            minuteLine = grandmothersLine * onePersonTimeMinute % minute;
+            Console.WriteLine(" Время ожидания " + (hourLine) + " часов " + (minuteLine) + " минут ");
+        }
+    }
+}
